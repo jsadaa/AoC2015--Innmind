ea2a5f9858ed240288542832ab0d16f0ef351d8b
Léo Paillard <103212024+jsadaa@users.noreply.github.com>
2023-09-25 17:27:25 +0200

    refacto day 1-2

--- solutions/d1-2.php
+++ solutions/d1-2.php
@@ -2,25 +2,41 @@
 
 declare(strict_types = 1);
 
+use Innmind\Immutable\Sequence;
 use Innmind\Immutable\Str;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$data = Str::of(file_get_contents('./../data/day1.txt'));
-$index =
-    $data
-        ->split()
-        ->map(static fn (Str $char) => $char->equals(Str::of('(')) ? 1 : -1)
-        ->map(static function (int $int) {
-            static $sum = 1;
-            $sum += $int;
-            if ($sum >= 0) return 1;
-            else return 0;
-        })
-        ->indexOf(0)
+$data = function() {
+    $data = \fopen(__DIR__.'./../data/day1.txt', 'r');
+
+    while ($line = \fgets($data)) {
+        yield Str::of($line);
+    }
+};
+
+$directions =
+    Sequence::lazy($data(...))
+        ->map(static fn (Str $line) => $line->split())
+        ->get(0)
         ->match(
-            static fn (int $index) => $index,
-            static fn () => null
+            static fn (Sequence $sequence): Sequence => $sequence,
+            static fn () => throw new \Exception('Unexpected value'),
+        )
+        ->map(static fn (Str $char) => $char->equals(Str::of('(')) ? 1 : -1);
+
+$sum = 0;
+for ($index = 1; $index < $directions->size(); $index++) {
+    $sum += $directions
+        ->get($index - 1)
+        ->match(
+            static fn (int $value): int => $value,
+            static fn () => throw new \Exception('Unexpected value'),
         );
+
+    if ($sum === -1) {
+        break;
+    }
+}
 
 print_r($index);
