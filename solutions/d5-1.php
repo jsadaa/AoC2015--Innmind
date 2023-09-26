0eb716cf8188d8cd664c38259b64b67e3e5aac22
Léo Paillard <103212024+jsadaa@users.noreply.github.com>
2023-09-26 22:49:48 +0200

    Implémentation day 5

---
+++ solutions/d5-1.php
@@ -0,0 +1,38 @@
+<?php
+
+use Innmind\Immutable\Sequence;
+use Innmind\Immutable\Str;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$data = function() {
+    $data = \fopen(__DIR__.'/../data/day5.txt', 'r');
+
+    while ($line = \fgets($data)) {
+        yield $line;
+    }
+};
+
+$strings = Sequence::lazy($data(...))
+    ->map(static fn (string $line) => Str::of($line));
+$badSubstrings = Sequence::of(Str::of('ab'), Str::of('cd'), Str::of('pq'), Str::of('xy'));
+
+$has3Vowels = static fn (Str $line) => $line->matches('/(?:[aeiou].*){3}/');
+$hasConsecutiveChars = static fn (Str $line) => $line->matches('/([a-zA-Z])\1/');
+$hasNoBadStrings = static function (Str $line) use ($badSubstrings) {
+    return !$badSubstrings->reduce(
+        false,
+        static function (bool $hasBadString, Str $badString) use ($line) {
+            $badString = $badString->toString();
+            return $hasBadString || $line->matches("/$badString/");
+        },
+    );
+};
+
+$nbrOfNiceStrings = $strings
+    ->filter($has3Vowels)
+    ->filter($hasConsecutiveChars)
+    ->filter($hasNoBadStrings)
+    ->count();
+
+print_r($nbrOfNiceStrings);
---
+++ solutions/d5-2.php
@@ -0,0 +1,30 @@
+<?php
+
+use Innmind\Immutable\Sequence;
+use Innmind\Immutable\Str;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$data = function() {
+    $data = \fopen(__DIR__.'/../data/day5.txt', 'r');
+
+    while ($line = \fgets($data)) {
+        yield $line;
+    }
+};
+
+$strings = Sequence::lazy($data(...))
+    ->map(fn (string $line) => Str::of($line));
+$vowels = Sequence::strings('a', 'e', 'i', 'o', 'u');
+$badSubstrings = Sequence::strings('ab', 'cd', 'pq', 'xy');
+
+$has2IdenticalPairs = static fn (Str $line) => $line->matches('/(?:(..).*\1)/');
+$has1LetterRepeatingWith1LetterBetween = static fn (Str $line) => $line->matches('/(?:(.).\1)/');
+
+$nbrOfNiceStrings = $strings
+    ->memoize()
+    ->filter($has2IdenticalPairs)
+    ->filter($has1LetterRepeatingWith1LetterBetween)
+    ->count();
+
+print_r($nbrOfNiceStrings);
